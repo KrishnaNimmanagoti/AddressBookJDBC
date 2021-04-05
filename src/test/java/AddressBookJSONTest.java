01e7b3d69292ab3b0af9833a728e56f4936391ca
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-04-05 21:29:19 +0530

    added to retrive entries of AddressBook from Json Server

---
+++ src/test/java/AddressBookJSONTest.java
@@ -0,0 +1,13 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AddressBookJSONTest {
+    @Test
+    public void testToRetrieveAllAddressBookData() {
+        Response response = RestAssured.get("http://localhost:3000/addressBook");
+        System.out.println(response.asString());
+        Assertions.assertEquals(200, response.getStatusCode());
+    }
+}
